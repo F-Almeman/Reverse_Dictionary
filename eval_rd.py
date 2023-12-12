d3b346265073e7f34a71683aca5ae9511aa9566e
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2023-12-12 09:36:39 +0000

    Update eval_rd.py

--- eval_rd.py
+++ eval_rd.py
@@ -4,9 +4,12 @@ import os
 import sys
 import csv
 from ast import literal_eval
+from sentence_transformers import SentenceTransformer,util
 import numpy as np
 import ast
 import torch
+from pathlib import Path
+import re
 
 # Evaluation metric
 def mean_reciprocal_rank(rs):
@@ -30,6 +33,35 @@ def mean_reciprocal_rank(rs):
     """
     rs = (np.asarray(r).nonzero()[0] for r in rs)
     return np.mean([1. / (r[0] + 1) if r.size else 0. for r in rs])
+    
+    
+def precision_at_k(r, k):
+    """Score is precision @ k
+    Relevance is binary (nonzero is relevant).
+    >>> r = [0, 0, 1]
+    >>> precision_at_k(r, 1)
+    0.0
+    >>> precision_at_k(r, 2)
+    0.0
+    >>> precision_at_k(r, 3)
+    0.33333333333333331
+    >>> precision_at_k(r, 4)
+    Traceback (most recent call last):
+        File "<stdin>", line 1, in ?
+    ValueError: Relevance score length < k
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+    Returns:
+        Precision @ k
+    Raises:
+        ValueError: len(r) must be >= k
+    """
+    assert k >= 1
+    r = np.asarray(r)[:k] != 0
+    if r.size != k:
+        raise ValueError('Relevance score length < k')
+    return np.mean(r)
 
 if __name__ == '__main__':
   # Create the parser
@@ -37,6 +69,8 @@ if __name__ == '__main__':
 
   parser.add_argument('-d','--dataset',help='The dataset',required=True)
   parser.add_argument('-e','--eval_method',help='The evaluation method',required=True)
+  parser.add_argument('-o', '--output_csv', help='Output CSV file', required=True)
+  
 
   
   args = parser.parse_args()
@@ -61,9 +95,23 @@ if __name__ == '__main__':
   
   if args.eval_method == "mrr":
     result = mean_reciprocal_rank(dataset['HITS'].to_list())
+  if re.match(r'^p\d+$', args.eval_method):
+    k = int(args.eval_method[1:])
+    result =  np.mean(dataset['HITS'].apply(lambda x: precision_at_k(x, k)).tolist())
     
-  with open('output.txt', 'w') as file:
-    file.write(f"Dataset: {args.dataset}\n")
-    file.write(f"Evaluation method: {args.eval_method}\n")
-    file.write(f"Result: {result}\n")
-    
+  output_data = {
+        'dataset': [Path(args.dataset).stem],
+        'method': [args.eval_method],
+        'result': [result]
+    }
+
+  output_df = pd.DataFrame(output_data)
+
+  # Check if the CSV file already exists
+  csv_file_path = args.output_csv
+  if Path(csv_file_path).is_file():
+    # If the file exists, append the new result as a new row
+    output_df.to_csv(csv_file_path, mode='a', header=False, index=False)
+  else:
+    # If the file does not exist, create a new file and write the header
+    output_df.to_csv(csv_file_path, index=False)
