1c2162fedcb5e4372c558d30601d3d2711d20aad
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2024-07-25 04:26:00 +0100

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
-    model_name = 'sentence-transformers/'+ args.model
+    model_name = 'args.model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = SentenceTransformer(model_name)
 
